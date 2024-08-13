9825171b38ff390931964d0034683f984b118634
basilkurian10 <basilkurian.ka@gmail.com>
2024-08-13 09:27:29 -0400

    added

--- chatter/src/app/page.jsx
+++ chatter/src/app/page.jsx
@@ -115,15 +115,7 @@ export default function MentalHealthChatbot() {
     </div>
     <div className="p-20 w-full flex flex-row justify-between">
     <div>
-      <iframe
-        width="740"
-        height="500"
-        src="https://www.youtube.com/watch?v=5oHnGu-TZ3E"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+     <iframe width="740" height="500" src="https://www.youtube.com/embed/5oHnGu-TZ3E?si=Vd19rxVSkyFDpmln" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
     <div>
       <iframe
